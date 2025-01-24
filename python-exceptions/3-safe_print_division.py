e148a0b302322c0f02f071dcbba332e65db30f5e
HamzaMoinet <moinethamza0@gmail.com>
2025-01-24 16:33:48 +0100

    modif task 2 and 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except Exception as e:
         result = None
     finally:
-        print("Inside result: {}:".format(result))
+        print("Inside result: {}".format(result))
     return result
