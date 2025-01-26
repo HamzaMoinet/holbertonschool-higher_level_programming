40689cd608750d979bf34c292d9502491edc59c6
HamzaMoinet <moinethamza0@gmail.com>
2025-01-26 11:02:52 +0100

    modif ligne 6

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,9 @@ def roman_to_int(roman_string):
     if roman_string is None:
         return 0
     roman_number = {
-        "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        "I": 1, "V": 5, "X": 10, "L": 50,
+        "C": 100, "D": 500, "M": 1000
+    }
     current_value = roman_number[roman_string[-1]]
     number = current_value
     for a in range(len(roman_string) - 1, 0, -1):
