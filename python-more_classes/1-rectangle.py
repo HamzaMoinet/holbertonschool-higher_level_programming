0940b61262d3f00fae6494646f8c8f56a713dcb6
HamzaMoinet <moinethamza0@gmail.com>
2025-01-29 11:02:07 +0100

    modif code

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,8 +21,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self._width = value
+        self._width = value
 
     @property
     def height(self):
@@ -36,15 +35,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self._height = value
-
-    def area(self):
-        """Calculate and return the area of the rectangle"""
-        return self._width * self._height
-
-    def perimeter(self):
-        """Calcule and return the perimeter of the rectangle"""
-        if self._width == 0 or self._height == 0:
-            return 0
-        return 2 * (self._width + self._height)
+        self._height = value
