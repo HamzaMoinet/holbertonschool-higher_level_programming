1ba99bcb004d47027133356d7e5eb46e8aba1fb5
HamzaMoinet <moinethamza0@gmail.com>
2025-01-29 14:20:10 +0100

    addnew fonction Detect instance deletion

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangle."""
+
+
+class Rectangle:
+    """Private attribute width and height"""
+
+    def __init__(self, width=0, height=0):
+        """"Initialise the rectangle"""
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        """Getter for width"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        """Getter for height"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
+
+    def area(self):
+        """Calculate the area of the rectangle"""
+        return self._height * self._width
+
+    def perimeter(self):
+        """Calcule the perimeter of the rectangle"""
+        if self._width == 0 or self._height == 0:
+            return 0
+        return 2 * (self._height + self._width)
+
+    def __str__(self):
+        """"Return a string representation of the rectangle using '#"""
+        if self._height == 0 or self._width == 0:
+            return ""
+        return "\n".join(["#" * self._width for _ in range(self._height)])
+
+    def __repr__(self):
+        """Return a string representation for debugging"""
+        return ("Rectangle({}, {})".format(self._width, self._height))
+
+    def __del__(self):
+        """Prints a message that delate regect"""
+        print("Bye rectangle...")
