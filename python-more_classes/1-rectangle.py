f03860c000e0fbebfa9359efba96dcd969a9b939
HamzaMoinet <moinethamza0@gmail.com>
2025-01-29 11:07:14 +0100

    modif code

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,13 +6,13 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """"Initialise the rectangle"""
-        self._width = width
-        self._height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
         """Getter for width"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -21,12 +21,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
         """Getter for height"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -35,4 +35,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
