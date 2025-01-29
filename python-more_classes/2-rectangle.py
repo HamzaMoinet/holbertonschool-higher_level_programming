c656189a61a3a4616163182912068f6786105b89
HamzaMoinet <moinethamza0@gmail.com>
2025-01-29 11:19:51 +0100

    modif task 2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -6,13 +6,13 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """"Initialise the rectangle"""
-        self.__width = width
-        self.__height = height
+        self._width = width
+        self._height = height
 
     @property
     def width(self):
         """Getter for width"""
-        return self.__width
+        return self._width
 
     @width.setter
     def width(self, value):
@@ -21,12 +21,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self._width = value
 
     @property
     def height(self):
         """Getter for height"""
-        return self.__height
+        return self._height
 
     @height.setter
     def height(self, value):
@@ -35,14 +35,14 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self._height = value
 
     def area(self):
         """Calculate the area of the rectangle"""
-        return self.__height * self.__width
+        return self._height * self._width
 
     def perimeter(self):
         """Calcule the perimeter of the rectangle"""
-        if self.__width == 0 or self.__height == 0:
+        if self._width == 0 or self._height == 0:
             return 0
-        return 2 * (self.__height + self.__width)
+        return 2 * (self._height + self._width)
