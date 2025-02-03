32dace0cf66883e9876d6a2b99431ae8f31507cf
HamzaMoinet <moinethamza0@gmail.com>
2025-02-03 16:30:12 +0100

    test

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-"""White fonction that returns the list"""
+"""Module to create class
+This module defines an empty class called list.
+"""
+
 
 def lookup(obj):
-    return(dir(obj))
+    """Empty class that defines a square.
+
+    This class doesn't currently have any properties, methods, or attributes.
+    It is essentially a placeholder for future development.
+    """
+
+    return dir(obj)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,12 @@
+
 #!/usr/bin/python3
+"""Module to create class
+"""
+
+
 class MyList(list):
-    """A subclass"""
+    """A subclass of list that can print a sorted version of itself."""
+
     def print_sorted(self):
-        """print the list"""
+        """Prints the list in sorted (ascending) order."""
         print(sorted(self))
