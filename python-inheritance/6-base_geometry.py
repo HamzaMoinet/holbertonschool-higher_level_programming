c17895ceef6bef0f3961c89164d7bca347b68b58
HamzaMoinet <moinethamza0@gmail.com>
2025-02-04 11:06:30 +0100

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+        """class BaseGeo"""
+
+        def area(self):
+              """Public instance"""
+              raise Exception("area() is not implemented")
