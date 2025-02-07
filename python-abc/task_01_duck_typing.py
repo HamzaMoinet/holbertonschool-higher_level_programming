a9566d55c437b9f85b1c8f60df6ba0aa66ad1141
HamzaMoinet <moinethamza0@gmail.com>
2025-02-07 14:06:33 +0100

    modif code

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Module to create class
-"""
+"""Module to create class"""
 
 
 from abc import ABC, abstractmethod
@@ -9,54 +8,53 @@ import math
 
 class Shape(ABC):
 
-    """Classe abstraite pour les formes géométriques."""
+    """Abstract class method"""
     @abstractmethod
     def area(self):
-        """Retourne l'aire de la forme."""
+        """Area shape"""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Retourne le périmètre de la forme."""
+        """Perimeter shape"""
         pass
 
 
 class Circle(Shape):
-    """Représente un cercle avec un rayon donné."""
+    """Class Circle from Shape"""
 
     def __init__(self, radius):
-        """Initialise le cercle avec un rayon."""
+        """Instantiation the radius of Circle """
 
         self.radius = radius
 
     def area(self):
-
-        """Calcule l'aire du cercle."""
+        """Area calcul of Circle"""
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        """Calcule le périmètre du cercle."""
+        """Perimeter calcul of Circle"""
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """Représente un rectangle avec une largeur et une hauteur."""
+    """Class Rectangle from Shape"""
 
     def __init__(self, width, height):
-        """Initialise le rectangle avec une largeur et une hauteur."""
+        """Instantiation the width and height of Rectangle """
         self.width = width
         self.height = height
 
     def area(self):
-        """Calcule l'aire du rectangle."""
+        """Area clacul of Rectangle"""
         return self.width * self.height
 
     def perimeter(self):
-        """Calcule le périmètre du rectangle."""
+        """perimeter calul of Rectangle"""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """Affiche l'aire et le périmètre de la forme."""
+    """Print the area and perimeter for Circle and Rectangle"""
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}")
