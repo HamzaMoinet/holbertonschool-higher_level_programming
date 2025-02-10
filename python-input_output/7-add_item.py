311acff841210e2308f0c1b6c6d56e427554c73a
HamzaMoinet <moinethamza0@gmail.com>
2025-02-10 15:40:26 +0100

    Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Load, add, save'''
+import json
+import sys
+
+
+def save_to_json_file(my_obj, filename):
+    '''function that writes an Object to a text file'''
+    with open(filename, "w") as outfile:
+        json.dump(my_obj, outfile)
+
+def load_from_json_file(filename):
+    '''function that creates an Object from a “JSON file”'''
+    with open(filename, "r", encoding="utf_8") as file:
+        return json.load(file)
+
+if __name__ == "__main__":
+    filename = "add_item.json"
+    try:
+        item = load_from_json_file(filename)
+    except FileNotFoundError:
+        item = []
+    item.extend(sys.argv[1:])
+    save_to_json_file(item, filename)
