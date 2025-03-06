02a73d1b648f23d3b0f65a36cbdb26b01912259e
HamzaMoinet <moinethamza0@gmail.com>
2025-03-06 11:17:13 +0100

    task 6 and modif comment

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- gggg
+-- creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- script for atribute all privilege
-GRANT ALL PRIVILEGES on *.* to 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,8 @@
--- gg
+-- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- gdgfs
+
+-- Create the user if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- dggregd
+
+-- Grant SELECT privilege on hbtn_0d_2 to user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
